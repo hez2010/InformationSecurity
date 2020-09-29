658fec85dbcff528834f4ba152967633bf027be5
Steven He <hez2010@outlook.com>
2020-09-29 17:15:02 +0800

    DES

---
+++ DES/DES.h
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#ifndef DES_H
+#define DES_H
+
+#include <stdbool.h>
+#include <malloc.h>
+#include <stdio.h>
+
+typedef unsigned long long uint64_t;
+typedef unsigned long uint32_t;
+typedef unsigned char uint8_t;
+
+uint64_t des_block(uint64_t data, uint64_t key, bool is_encrypt);
+void des_file(char* in_filename, char* out_filename, const char* key_filename, bool is_encrypt);
+
+#endif
